4fe76d746fd24163261255c5ad52798f874e5c4c
MukkuChemjong <91582471+MukkuChemjong@users.noreply.github.com>
2025-09-27 01:07:19 -0400

    swap database url to local

--- backend/settings.py
+++ backend/settings.py
@@ -67,10 +67,10 @@ MIDDLEWARE = [
 
 # Database
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',   # fallback for local dev
         conn_max_age=600,
-        ssl_require=False,
+        ssl_require=False
     )
 }
 
